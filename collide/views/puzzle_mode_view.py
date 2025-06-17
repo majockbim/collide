30f31ef8dfbfed86b7971d662df497e50cd2f0c6
majockbim <majock.bim1@gmail.com>
2025-06-16 20:18:00 -0400

    fixed text

--- collide/views/puzzle_mode_view.py
+++ collide/views/puzzle_mode_view.py
@@ -5,5 +5,5 @@ def puzzle_view(page: ft.Page):
     page.title = "Puzzle Mode"
     page.controls.clear()
     page.controls.append(back_button(page))
-    page.controls.append(ft.Text("Puzzle mode coming soon!"))
+    page.controls.append(ft.Text("Puzzle mode coming soon!", text_align=ft.TextAlign.CENTER, size=24, color="#e52c34"))
     page.update()
--- collide/views/simulation_mode_view.py
+++ collide/views/simulation_mode_view.py
@@ -5,5 +5,5 @@ def simulation_view(page: ft.Page):
     page.title = "Simulation Mode"
     page.controls.clear()
     page.controls.append(back_button(page))
-    page.controls.append(ft.Text("Simulation mode coming soon!"))
+    page.controls.append(ft.Text("Simulation mode coming soon!", text_align=ft.TextAlign.CENTER, size=24, color="#e52c34"))
     page.update()
