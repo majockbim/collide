3184d2c9dd637ffd933e2b3dbccd272f3d0e4019
majockbim <majock.bim1@gmail.com>
2025-06-19 21:41:26 -0400

    fixed error

--- collide/views/simulation_mode_view.py
+++ collide/views/simulation_mode_view.py
@@ -4,6 +4,7 @@ from collide.components.back_button import back_button
 def simulation_view(page: ft.Page):
     page.title = "Simulation Mode"
     page.controls.clear()
-    page.controls.append(back_button(page))
     page.controls.append(ft.Text("Simulation mode coming soon!", text_align=ft.TextAlign.CENTER, size=24, color="#e52c34"))
+
+    
     page.update()
