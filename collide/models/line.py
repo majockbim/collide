3ab108e4f89fe033ab145d6f010cdb4604c21183
majockbim <majock.bim1@gmail.com>
2025-06-20 20:02:59 -0400

    removed file not in use

--- collide/models/line.py
+++
@@ -1,9 +0,0 @@
-# Line model - currently unused but keeping for future expansion
-class Line:
-    def __init__(self, x1, y1, x2, y2, color="#2b2d2e", width=3):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-        self.color = color
-        self.width = width
